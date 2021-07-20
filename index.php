a9f708fbf936169d38431fa7faae2196bce1269c
Roshan <rytaneja@gmail.com>
2021-07-20 01:00:24 -0700

    Add the Loginform to the application

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
+<?php
+session_start();
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+ 
+function loginForm(){
+    echo'
+    <div id="loginform">
+      <p>Please enter your name to continue!</p>
+      <form action="index.php" method="post">
+        <label for="name">Name &mdash;</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+      </form>
+    </div>
+    ';
+}
+?>
+
 <!DOCTYPE html>
+
 <html lang="en">
     <head>
         <meta charset="utf-8" />
