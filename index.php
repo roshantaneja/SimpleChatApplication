28ad1298881b94cd11304b7ce75118cc9dbcedee
Roshan <rytaneja@gmail.com>
2021-07-20 01:27:18 -0700

    Fixed login issues

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
+ 
 session_start();
+ 
+if(isset($_GET['logout'])){    
+     
+    //Simple exit message
+    $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>". $_SESSION['name'] ."</b> has left the chat session.</span><br></div>";
+    file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
  
 if(isset($_POST['enter'])){
     if($_POST['name'] != ""){
@@ -11,17 +22,17 @@ if(isset($_POST['enter'])){
 }
  
 function loginForm(){
-    echo'
-    <div id="loginform">
-      <p>Please enter your name to continue!</p>
-      <form action="index.php" method="post">
-        <label for="name">Name &mdash;</label>
-        <input type="text" name="name" id="name" />
-        <input type="submit" name="enter" id="enter" value="Enter" />
-      </form>
-    </div>
-    ';
+    echo
+    '<div id="loginform">
+    <p>Please enter your name to continue!</p>
+    <form action="index.php" method="post">
+      <label for="name">Name &mdash;</label>
+      <input type="text" name="name" id="name" />
+      <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+  </div>';
 }
+ 
 ?>
 
 <!DOCTYPE html>
